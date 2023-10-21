735829d236e93ebf73e1ba981d00119fb2afe2a4
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2023-10-21 12:02:14 +0200

    refactor: correct return type & route configuration

--- backend/Contacts.WebAPI/Contacts.WebAPI/Controllers/ContactsController.cs
+++ backend/Contacts.WebAPI/Contacts.WebAPI/Controllers/ContactsController.cs
@@ -1,9 +1,11 @@
-﻿using Contacts.WebAPI.Infrastructure;
+﻿using Contacts.WebAPI.DTOs;
+using Contacts.WebAPI.Infrastructure;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Contacts.WebAPI.Controllers;
 
 [ApiController]
+[Route("api/contacts")]
 public class ContactsController : ControllerBase
 {
     private readonly DataService _dataService;
@@ -13,11 +15,17 @@ public class ContactsController : ControllerBase
         _dataService = dataService;
     }
 
-    [HttpGet("api/contacts")]
-    public IActionResult Get()
+    [HttpGet()]
+    public ActionResult<IEnumerable<ContactDto>> Get()
     {
-        return new JsonResult(
-            _dataService.Contacts
-        );
+        var contactsDto = _dataService.Contacts.Select(c => new ContactDto
+        {
+            Id = c.Id,
+            FirstName = c.FirstName,
+            LastName = c.LastName,
+            Email = c.Email
+        });
+
+        return Ok(contactsDto);
     }
 }
