4886139e306edf6c551040cdc91e096845c43adf
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2023-10-21 15:36:44 +0200

    feat: get single phone

--- backend/Contacts.WebAPI/Contacts.WebAPI/Controllers/PhonesController.cs
+++ backend/Contacts.WebAPI/Contacts.WebAPI/Controllers/PhonesController.cs
@@ -15,6 +15,7 @@ namespace Contacts.WebAPI.Controllers
             _dataService = dataService;
         }
 
+        // GET api/contacts/1/phones
         [HttpGet]
         public ActionResult<IEnumerable<PhoneDto>> GetPhones(int contactId)
         {
@@ -34,6 +35,34 @@ namespace Contacts.WebAPI.Controllers
                 });
 
             return Ok(phonesDto);
+        }
+
+        // GET api/contacts/1/phones/1
+        [HttpGet("{phoneId:int}")]
+        public ActionResult<PhoneDto> GetPhone(int contactId, int phoneId)
+        {
+            var contact = _dataService.Contacts.SingleOrDefault(c => c.Id == contactId);
+
+            if (contact is null)
+            {
+                return NotFound();
+            }
+
+            var phone = contact.Phones.SingleOrDefault(p => p.Id == phoneId);
+
+            if (phone is null)
+            {
+                return NotFound();
+            }
+
+            var phoneDto = new PhoneDto()
+            {
+                Id = phone.Id,
+                Number = phone.Number,
+                Description = phone.Description
+            };
+
+            return Ok(phoneDto);
         }
     }
 }
