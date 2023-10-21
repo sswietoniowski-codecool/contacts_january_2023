cebab633f3d591a56e9eab59a6ab60f70aed8627
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2023-10-21 11:30:09 +0200

    feat: add 1st controller

---
+++ backend/Contacts.WebAPI/Contacts.WebAPI/Controllers/ContactsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Contacts.WebAPI.Controllers;
+
+[ApiController]
+public class ContactsController : Controller
+{
+    [HttpGet("api/contacts")]
+    public IActionResult Get()
+    {
+        return new JsonResult(
+            new List<object>()
+            {
+                new { Id = 1, FirstName = "Jan", LastName = "Kowalski", Email = "jkowalski@u.pl"},
+                new { Id = 2, FirstName = "Adam", LastName = "Nowak", Email = "anowak@u.pl"}
+            }
+        );
+    }
+}
