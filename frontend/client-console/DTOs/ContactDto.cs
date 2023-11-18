a2249f7ee5ee9d44b58427122c251d6c973f0373
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2023-11-18 09:52:43 +0100

    feat: create project & add 1st DTO

---
+++ frontend/client-console/DTOs/ContactDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Client.Console.DTOs;
+
+public class ContactDto
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+}
